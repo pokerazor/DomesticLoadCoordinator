b606d97ed7891e7739af3c1cf825a77beb17cd3f
friesenkiwi <friesenkiwi@lappok>
2017-05-29 12:38:48 +0200

    Work on dependencies and add agent class

---
+++ src/main/java/de/unidue/stud/sehawagn/energy/GroupCoordinatorAgent.java
@@ -0,0 +1,33 @@
+package de.unidue.stud.sehawagn.energy;
+
+import hygrid.agent.AbstractEnergyAgent;
+import hygrid.agent.AbstractIOReal;
+import hygrid.agent.AbstractIOSimulated;
+import hygrid.agent.AbstractInternalDataModel;
+
+public class GroupCoordinatorAgent extends AbstractEnergyAgent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4265865470470961177L;
+
+	@Override
+	public AbstractIOReal getIOReal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AbstractIOSimulated getIOSimulated() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AbstractInternalDataModel getInternalDataModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
