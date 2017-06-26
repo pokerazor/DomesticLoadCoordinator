4bef7feb9acc64f3b52aa6b6383490283446c541
pokerazor <hanno-felix.wagner@stud.uni-due.de>
2017-06-26 15:44:59 +0200

    Debug and make GroupCoordinatorAgent communication run

--- src/main/java/de/unidue/stud/sehawagn/energy/RequestRunSchedule.java
+++ src/main/java/de/unidue/stud/sehawagn/energy/RequestRunSchedule.java
@@ -1,14 +1,18 @@
 package de.unidue.stud.sehawagn.energy;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.Vector;
 
 import energy.optionModel.TechnicalSystem;
+import jade.core.AID;
 import jade.core.Agent;
 import jade.domain.FIPANames;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.lang.acl.UnreadableException;
 import jade.proto.AchieveREInitiator;
+import jade.util.leap.Iterator;
 
 public class RequestRunSchedule extends AchieveREInitiator {
 
@@ -16,6 +20,27 @@ public class RequestRunSchedule extends AchieveREInitiator {
 
 	public RequestRunSchedule(Agent a, ACLMessage msg) {
 		super(a, msg);
+		Iterator receivers = msg.getAllIntendedReceiver();
+		while (receivers.hasNext()) {
+			AID recvAID = (AID) receivers.next();
+			System.out.println("message intended for "+recvAID);
+		}
+		
+		Vector<ACLMessage> allInitMessages = (Vector<ACLMessage>) getDataStore().get(ALL_INITIATIONS_K);
+		if(allInitMessages!=null){
+			System.out.println("sent messages");
+			for (ACLMessage aclMessage : allInitMessages) {
+				Iterator actualreceivers = aclMessage.getAllIntendedReceiver();
+				while (actualreceivers.hasNext()) {
+					AID recvAID = (AID) actualreceivers.next();
+					System.out.println("message sent to "+recvAID);
+				}
+			}
+		}
+
+		
+		System.out.println("RequestRunSchedule constructor");
+
 	}
 
 	public static ACLMessage prepareRequestMessage(Agent a, Serializable content) {
@@ -23,7 +48,11 @@ public class RequestRunSchedule extends AchieveREInitiator {
 		Helper.fillMessage(msg, content, a);
 		msg.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
 
-		System.out.println("RequestRunSchedule from Domestic Load Coordinator Agent ");
+		// We want to receive a reply in 10 secs
+		msg.setReplyByDate(new Date(System.currentTimeMillis() + 10000));
+
+
+		System.out.println("RequestRunSchedule prepareRequestMessage");
 
 		return msg;
 	}
